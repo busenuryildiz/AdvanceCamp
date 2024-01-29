ff39fc17b1233eda17fcd2442095ee86f0e3e96c
busenuryildiz <busenuryildiz5337@gmail.com>
2024-01-30 00:42:45 +0300

    creation of fiction and architecture

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,9 +1,12 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
