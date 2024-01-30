fef8d7e9c88787b6a065cdd519ae65f47675cd62
busenuryildiz <busenuryildiz5337@gmail.com>
2024-01-30 21:39:31 +0300

    CQRS

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Features.Brands.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommand :IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }
+        public class CreatedBrandCommandHandler:IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+           private readonly IBrandRepository _brandRepository;
+           private readonly IMapper _mapper;
+
+           public CreatedBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+           {
+               _brandRepository = brandRepository;
+               _mapper = mapper;
+           }
+
+           public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+           {
+               Brand mappedBrand = _mapper.Map<Brand>(request);
+               Brand createdBrand = await _brandRepository.AddAsync(mappedBrand);
+               CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+              
+               return createdBrandDto;
+           }
+        }
+
+    }
+}
--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Application;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,8 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
+//builder.Services.AddSecurityServices();
+//builder.Services.AddInfrastructureServices();
+builder.Services.AddHttpContextAccessor();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
